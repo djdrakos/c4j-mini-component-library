15742cce809573fec3c9dc5f7e8163830cc426b1
DJ Drakos <d.josi.drakos@gmail.com>
2023-03-20 16:33:10 -0700

    Add Select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,56 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  appearance: none;
+`
+
+const StyledSelect = styled.div`
+  display: flex;
+  gap: 20px;
+  align-items: center;
+  width: max-content;
+  height: calc(1rem + 24px);
+  padding: 12px 16px;
+  background: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  color: ${COLORS.gray700};
+  font-size: 1rem;
+
+  ${NativeSelect}:hover + &{
+    color: ${COLORS.black};
+  }
+
+  ${NativeSelect}:focus + &{
+    outline: 5px auto Highlight;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+`
+
+const Wrapper = styled.div`
+  width: max-content;
+  position: relative;
+`
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} displayedValue={displayedValue} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <StyledSelect>
+        {displayedValue}
+        <Icon id={'chevron-down'} size={24} strokeWidth={1.5} />
+      </StyledSelect>
+    </Wrapper>
   );
 };
 
