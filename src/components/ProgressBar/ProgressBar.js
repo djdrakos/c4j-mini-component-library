96e40db8ff9351c48dd332027e3b75a392b686f4
DJ Drakos <d.josi.drakos@gmail.com>
2023-03-20 14:27:06 -0700

    Refactor ProgressBar component

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -2,107 +2,82 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
-// import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBase = styled.progress`
+const SIZES = {
+  small: {
+    '--height': 8 + 'px',
+    '--border-radius': 4 + 'px',
+    '--padding': 0,
+  },
+  medium: {
+    '--height': 12 + 'px',
+    '--border-radius': 4 + 'px',
+    '--padding': 0,
+  },
+  large: {
+    '--height': 24 + 'px',
+    '--border-radius': 8 + 'px',
+    '--padding': 4 + 'px',
+  },
+}
+
+const StyledProgress = styled.progress`
   -webkit-appearance: none;
   -moz-appearance: none;
   appearance: none; 
-  width: 370px;
+  height: var(--height);
+  width: 100%;
+  padding: var(--padding);
   background: ${COLORS.transparentGray15};
   border: none;
+  border-radius: var(--border-radius);
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  overflow: hidden;
 
+/* Styles for Webkit browsers */
   ::-webkit-progress-bar {
-    background: inherit;
-  }
-  
-  ::-moz-progress-bar {
-    border-radius: 4px 0px 0px 4px;
-    background: ${COLORS.primary};
+    background: transparent;
+    border-radius: 4px;
+    overflow: hidden;
   }
 
   ::-webkit-progress-value {
-    border-radius: 4px 0px 0px 4px;
     background: ${COLORS.primary};
+    border-radius: 4px 0px 0px 4px;
   }
 
-  &[value="99"] {
-    ::-moz-progress-bar {
-      border-radius:  4px 2px 2px 4px;
-    }
-
-    ::-webkit-progress-value {
-      border-radius:  4px 2px 2px 4px;
-    }
+/* Styles for Mozilla Browsers, the <progress> tag generates one pseudo element: "-moz-progress-bar".*/
+  ::-moz-progress-bar {
+    background: ${COLORS.primary};
+    border-radius: 4px 0px 0px 4px;
   }
-  
+
+/* Hacky workaround to trim corners of progress value on large size bar in Mozilla browsers */
   &[value^="99."] {
     ::-moz-progress-bar {
       border-radius:  4px 3px 3px 4px;
     }
-
-    ::-webkit-progress-value {
-      border-radius:  4px 3px 3px 4px;
-    }
   }
-  
+
   &[value="100"] {
     ::-moz-progress-bar {
       border-radius: 4px;
     }
-
-    ::-webkit-progress-value {
-      border-radius: 4px;
-    }
-  }
-`
-const ProgressSmall = styled(ProgressBase)`
-  height: 8px;
-  border-radius: 4px;
-  
-  ::-webkit-progress-bar {
-    border-radius: 4px;
-  }
-  `
-const ProgressMedium = styled(ProgressBase)`
-  height: 12px;
-  border-radius: 4px;
-  ::-webkit-progress-bar {
-    border-radius: 4px;
-  }
-  `
-const ProgressLarge = styled(ProgressBase)`
-  height: 24px;
-  border-radius: 8px;
-  padding: 4px;
-
-  ::-webkit-progress-bar {
-    border-radius: 8px;
   }
 `
 
 const ProgressBar = ({ label='Loading Progress', value, size='medium'}) => {
-  const Progress = ({children, ...props}) => {
-    if(size === 'small') {
-      return <ProgressSmall {...props}>{children}</ProgressSmall>
-    }
-    if(size === 'large') {
-      return <ProgressLarge {...props}>{children}</ProgressLarge>
-    }
-    if(size === 'medium') {
-      return <ProgressMedium {...props}>{children}</ProgressMedium>
-    }
-    else throw new Error(`Invalid value passed for "size" prop: ${size}. The ProgressBar component supports the following sizes: "small", "medium", "large".`)
-    }
+  const styles = SIZES[size]
+  if (!styles) throw new Error(`Invalid value passed for "size" prop: ${size}. The ProgressBar component supports the following sizes: "small", "medium", "large".`)
 
-  return <Progress 
+  return <StyledProgress 
     aria-label={label}
     aria-valuenow={value}
     max='100'
     size={size}
+    style={styles}
     value={value}
-  ></Progress>
+  ></StyledProgress>
 }
 
 export default ProgressBar;
